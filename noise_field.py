fda003cf918d624e9cde99eb92dd8a7b288ed096
Jack <jackwarren430@gmail.com>
2023-03-27 23:00:41 -0400

    start

---
+++ noise_field.py
@@ -0,0 +1,55 @@
+import glfw
+from OpenGL.GL import *
+import numpy as np
+import noise
+
+class NoiseField:
+    def __init__(self):
+        self.WIDTH = 640
+        self.HEIGHT = 480
+        self.increment = 0.1
+
+    def main(self):
+        # Initialize GLFW
+        if not glfw.init():
+            return
+
+        # Create a window
+        window = glfw.create_window(self.WIDTH, self.HEIGHT, "My OpenGL Window", None, None)
+
+        # Make the window the current context
+        glfw.make_context_current(window)
+
+        # Loop until the user closes the window
+        while not glfw.window_should_close(window):
+            glClearColor(0, 0, 0, 1)
+            glClear(GL_COLOR_BUFFER_BIT)
+
+            self.generate_noise()
+
+            glfw.swap_buffers(window)
+            glfw.poll_events()
+
+        # Destroy the window
+        glfw.destroy_window(window)
+
+        # Terminate GLFW
+        glfw.terminate()
+
+    def generate_noise(self):
+        y_off = 0
+        for y in range(self.HEIGHT):
+            x_off = 0
+            for x in range(self.WIDTH):
+                r = noise.pnoise2(x_off, y_off) * 255
+                glColor(r, r, r)
+                glBegin(GL_POITNS)
+                glVertex2f(x, y)
+                glEnd()
+                x_off += self.increment
+            y_off += self.increment
+
+
+if __name__ == "__main__":
+    window = NoiseField()
+    window.main()
